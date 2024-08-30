da0e4829bcecedd3224d52c44191df5ca1ebd1d7
Yeremy Molina <yeremymolina14@gmail.com>
2024-08-30 10:18:35 -0400

    app3 terminada

--- django_git/urls.py
+++ django_git/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/',include("app1.urls")),
     path('app2/',include("app2.urls")),
+    path('app3/',include("app3.urls")),
     
 ]
